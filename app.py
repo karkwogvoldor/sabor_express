c4c0329dddd7b14a8bfe37078e877a78f5647272
Jerônimo <karkwogvoldor@gmail.com>
2026-03-30 15:49:08 -0300

    melhorando sintaxe com funcoes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+      print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -6,19 +9,32 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       Bem-vindo ao Sabor Express! O melhor serviço de entrega de comida da cidade.\n""")
+def exibir_opcoes():   
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurante')
+      print('3. Ativar restaurante')
+      print('4. Sair\n')
+def finalizar_app():
+    os.system('cls')
+    print('Obrigado por usar o Sabor Express! Até a próxima!')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    elif opcao_escolhida == 4:  # <-- Agora o 4 tem o seu lugar exclusivo!
+        finalizar_app()
+    else:                       # <-- O else agora só pega números errados
+        print('Opção inválida! Tente novamente.\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
-if opcao_escolhida == '1':
-    print('Cadastrar restaurante')
-elif opcao_escolhida == '2':
-    print('Listar restaurante')
-elif opcao_escolhida == '3':
-    print('Ativar restaurante')
-else:    
-     print('Encerrando Programa')
+if __name__ == '__main__':
+    main()
